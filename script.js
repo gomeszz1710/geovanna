67037f5587f85016fa77ce39037dde2fb3d569cb
gomeszz1710 <geovanna.soares.souza@escola.pr.gov.br>
2025-06-13 11:55:28 -0300

    rrrg

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Qual seu estilo de vida?",
         alternativas: [
             {
-                texto: "cAMPO",
+                texto: "campo",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "cidade",
                 afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
